5fff5ffb512b845e04c1704b74bb073360863aec
Jacek <jacek.s.gajek@gmail.com>
2018-11-06 17:15:31 +0100

    add frotnend

---
+++ frontend/src/app/test/test.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('init');
+  }
+
+  public loadQuestion() {
+    console.log('loadQuestion');
+  }
+}
